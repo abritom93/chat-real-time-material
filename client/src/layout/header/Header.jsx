46f5046464048254b0184d3a137a977d6f1ff1e2
Ariel Brito <abritom93@gmail.com>
2023-11-11 09:38:17 +0100

    Adjust to bottom the actions buttons

--- client/src/layout/header/Header.jsx
+++ client/src/layout/header/Header.jsx
@@ -44,7 +44,7 @@ const Header = ({setIsOpenNavigation}) => {
                             <MenuIcon/>
                         </IconButton>
                         <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                            News
+                            Chat
                         </Typography>
                         {
                             user ?
